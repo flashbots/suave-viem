1e7a94b0f9d56580716658a628276d721890de1a
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-10-25 15:33:31 -0700

    add test:e2e in playgrounds/bun/

--- playgrounds/bun/index.ts
+++ playgrounds/bun/index.ts
@@ -1,5 +1,13 @@
-import { http, createPublicClient } from 'viem'
-import { mainnet, polygon } from 'viem/chains'
+import {
+  http,
+  Hex,
+  createPublicClient,
+  createWalletClient,
+  zeroAddress,
+} from 'viem'
+import { privateKeyToAccount } from 'viem/accounts'
+import { mainnet, polygon, suaveRigil } from 'viem/chains'
+import { SuaveTransactionRequest } from 'viem/chains/suave/types'
 
 ////////////////////////////////////////////////////////////////////
 // Clients
@@ -12,6 +20,14 @@ export const publicClients = {
   polygon: createPublicClient({
     chain: polygon,
     transport: http(),
+  }),
+  suaveRigil: createPublicClient({
+    chain: suaveRigil,
+    transport: http(),
+  }),
+  suaveLocal: createPublicClient({
+    chain: suaveRigil,
+    transport: http(suaveRigil.rpcUrls.local.http[0]),
   }),
 }
 
@@ -28,13 +44,35 @@ export const publicClients = {
 // const logs = await publicClients.mainnet.getLogs()
 // console.log(logs)
 
-publicClients.mainnet.watchEvent({
-  onError(error) {
-    console.log(error)
-  },
-  onLogs(logs) {
-    console.log(logs)
-  },
+// publicClients.mainnet.watchEvent({
+//   onError(error) {
+//     console.log(error)
+//   },
+//   onLogs(logs) {
+//     console.log(logs)
+//   },
+// })
+
+const suaveTxReq: SuaveTransactionRequest = {
+  executionNode: zeroAddress,
+  isConfidential: true,
+  confidentialInputs: '0x13',
+  from: zeroAddress,
+  to: zeroAddress,
+  type: 'suave',
+  gasPrice: 10000000000n,
+  gas: 21000n,
+  /* TODO: modify default gasPrice and gas
+  - not defining them makes eth_estimateGas try & fail
+  - it stringifies integer values without hexifying them */
+}
+const wallet = createWalletClient({
+  account: privateKeyToAccount(process.env.PRIVATE_KEY! as Hex),
+  transport: http(suaveRigil.rpcUrls.local.http[0]),
+  chain: suaveRigil,
 })
+// publicClients.suaveRigil.sendRawTransaction(suaveTxReq)
+const res = await wallet.sendTransaction(suaveTxReq)
+console.log(`sent tx: ${res}`)
 
 ////////////////////////////////////////////////////////////////////
