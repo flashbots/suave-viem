51f382eb11a35f5e202d84f49012676c9bbaa02c
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-10-30 23:55:41 -0700

    fix rlp signature bugs; working CCR transactionRequests

--- src/chains/suave/serializers.ts
+++ src/chains/suave/serializers.ts
@@ -21,7 +21,7 @@ import {
 import { toTransactionArray } from '~viem/utils/transaction/parseTransaction.js'
 import type { Hex, Signature } from '../../types/misc.js'
 import { concatHex } from '../../utils/data/concat.js'
-import { toHex } from '../../utils/encoding/toHex.js'
+import { numberToHex, toHex } from '../../utils/encoding/toHex.js'
 import { toRlp } from '../../utils/encoding/toRlp.js'
 import { keccak256 } from '../../utils/hash/keccak256.js'
 import {
@@ -99,17 +99,17 @@ export const serializeConfidentialRecord = (
         })
     */
 
-    nonce ? toHex(nonce) : '0x',
-    gasPrice ? toHex(gasPrice) : '0x',
-    gas ? toHex(gas) : '0x',
+    nonce ? numberToHex(nonce) : '0x',
+    gasPrice ? numberToHex(gasPrice) : '0x',
+    gas ? numberToHex(gas) : '0x',
     to ?? '0x',
-    value ? toHex(value) : '0x',
+    value ? numberToHex(value) : '0x',
     data ?? '0x', // data
 
     executionNode,
     confidentialInputs ? keccak256(confidentialInputs) : '0x',
 
-    chainId ? toHex(chainId) : '0x',
+    chainId ? numberToHex(chainId) : '0x',
     // r ?? '0x',
     // s ?? '0x',
     // v !== undefined ? (v === 27n ? '0x' : '0x1') : '0x',
@@ -125,8 +125,8 @@ export const serializeConfidentialRecord = (
     //   throw new Error(`signature mismatch v=${signature.v} vs v=${v}`)
     serializedTransaction.push(
       signature.v === 27n ? '0x' : '0x01', // yParity
-      toHex(signature.r),
-      toHex(signature.s),
+      signature.r,
+      signature.s,
     )
   }
 
@@ -139,47 +139,40 @@ export const serializeConfidentialRecord = (
   ]) as TransactionSerializedSuave
 }
 
-export const serializeTransactionSuave = (
-  transaction: TransactionSerializableSuave,
-  signedComputeRecord: Hex,
-  _signature?: Signature,
+const safeHexToBigInt = (hex: Hex) => {
+  if (hex === '0x') return 0n
+  return hexToBigInt(hex)
+}
+
+const safeHexToNumber = (hex: Hex) => {
+  if (hex === '0x') return 0
+  return hexToNumber(hex)
+}
+
+const deserializeSignedComputeRecord = (
+  signedComputeRecord: Hex
 ) => {
-  if (transaction.type !== SuaveTxTypes.ConfidentialRequest) {
-    throw new Error('Invalid transaction type') // TODO: make this a custom error
-  }
-  const {
-    // r,
-    // s,
-    // v,
+  console.log('signedComputeRecortd', signedComputeRecord)
+  const transactionArray = toTransactionArray(signedComputeRecord)
+
+  const [
     nonce,
     gasPrice,
     gas,
     to,
     value,
     data,
-    chainId,
     executionNode,
-    confidentialInputs,
     confidentialInputsHash,
-  } = transaction
-
-  const transactionArray = toTransactionArray(signedComputeRecord)
-
-  const [
-    nonce_,
-    gasPrice_,
-    gas_,
-    to_,
-    value_,
-    data_,
-    executionNode_,
-    confidentialInputsHash_,
-    chainId_,
+    chainId,
     v,
     r,
     s,
   ] = transactionArray
 
+  console.log('v', v)
+  console.log('r', r)
+  console.log('s', s)
   if (transactionArray.length !== 12) {
     throw new InvalidSerializedTransactionError({
       attributes: {
@@ -201,88 +194,62 @@ export const serializeTransactionSuave = (
     })
   }
 
-  console.log('signedComputeRecortd', signedComputeRecord)
-  console.log('value_', value_)
-
-  const safeHexToBigInt = (hex: Hex) => {
-    if (hex === '0x') return 0n
-    return hexToBigInt(hex)
-  }
-
-  const safeHexToNumber = (hex: Hex) => {
-    if (hex === '0x') return 0
-    return hexToNumber(hex)
-  }
-
-  console.log('mkay', {
-    nonce: safeHexToNumber(nonce_ as Hex),
-    to: to_ as Hex,
-    data: data_ as Hex,
-    gas: hexToBigInt(gas_ as Hex),
-    executionNode: executionNode_ as Hex,
-    confidentialInputsHash: confidentialInputsHash_ as Hex,
-    value: safeHexToBigInt(value_ as Hex),
-    gasPrice: safeHexToBigInt(gasPrice_ as Hex),
-    chainId: hexToNumber(chainId_ as Hex),
-    v: safeHexToBigInt(v as Hex),
-    r: r as Hex,
-    s: s as Hex,
-  })
   const ccRecord: Partial<TransactionSerializableSuave> = {
-    nonce: safeHexToNumber(nonce_ as Hex),
-    to: to_ as Hex,
-    data: data_ as Hex,
-    gas: hexToBigInt(gas_ as Hex),
-    executionNode: executionNode_ as Hex,
-    confidentialInputsHash: confidentialInputsHash_ as Hex,
-    value: safeHexToBigInt(value_ as Hex),
-    gasPrice: safeHexToBigInt(gasPrice_ as Hex),
-    chainId: hexToNumber(chainId_ as Hex),
+    nonce: safeHexToNumber(nonce as Hex),
+    to: to as Hex,
+    data: data as Hex,
+    gas: hexToBigInt(gas as Hex),
+    executionNode: executionNode as Hex,
+    confidentialInputsHash: confidentialInputsHash as Hex,
+    value: safeHexToBigInt(value as Hex),
+    gasPrice: safeHexToBigInt(gasPrice as Hex),
+    chainId: hexToNumber(chainId as Hex),
     v: safeHexToBigInt(v as Hex),
     r: r as Hex,
     s: s as Hex,
   }
+  console.log('mkay', ccRecord)
 
-  console.log('ccrecord', ccRecord)
 
+  return ccRecord
+}
+
+export const serializeTransactionSuave = (
+  transaction: TransactionSerializableSuave,
+  signedComputeRecord: Hex,
+  _signature?: Signature,
+) => {
+  console.log("full tx", transaction)
+  if (transaction.type !== SuaveTxTypes.ConfidentialRequest) {
+    throw new Error('Invalid transaction type') // TODO: make this a custom error
+  }
+  const ccRecord = deserializeSignedComputeRecord(signedComputeRecord)
+  console.log('confidentialInputs', transaction.confidentialInputs)
+  const confidentialInputsHash = keccak256(transaction.confidentialInputs || '0x')
+  console.log('confidentialInputsHash (calc)', confidentialInputsHash)
+  console.log('confidentialInputsHash (ccr)', ccRecord.confidentialInputsHash)
+  console.log('confidentialInputsHash (tx)', transaction.confidentialInputsHash)
   const serializedTransaction: (Hex | Hex[])[] = [
     [
-      ccRecord.nonce ? toHex(ccRecord.nonce) : '0x',
+      ccRecord.nonce ? numberToHex(ccRecord.nonce) : '0x',
       ccRecord.gasPrice ? toHex(ccRecord.gasPrice) : '0x',
       ccRecord.gas ? toHex(ccRecord.gas) : '0x',
       ccRecord.to ?? '0x',
       ccRecord.value ? toHex(ccRecord.value) : '0x',
       ccRecord.data ?? '0x', // data
 
-      executionNode,
-      confidentialInputs
-        ? keccak256(confidentialInputs)
-        : confidentialInputsHash || '0x',
+      ccRecord.executionNode ?? '0x',
+      transaction.confidentialInputs
+        ? keccak256(transaction.confidentialInputs)
+        : ccRecord.confidentialInputsHash || '0x',
 
-      chainId ? toHex(chainId) : '0x',
-      // if (signature) {
-      // if (r && signature.r !== r)
-      //   throw new Error(`signature mismatch r=${signature.r} vs r=${r}`)
-      // if (s && signature.s !== s)
-      //   throw new Error(`signature mismatch s=${signature.s} vs s=${s}`)
-      // if (v && signature.v !== v)
-      //   throw new Error(`signature mismatch v=${signature.v} vs v=${v}`)
+      ccRecord.chainId ? numberToHex(ccRecord.chainId) : '0x',
       ccRecord.v === 27n ? '0x' : '0x1', // yParity
-      toHex(ccRecord.r ?? '0x'),
-      toHex(ccRecord.s ?? '0x'),
+      ccRecord.r as Hex,
+      ccRecord.s as Hex,
     ],
-
-    confidentialInputs ?? '0x',
+    transaction.confidentialInputs ?? '0x',
   ]
-
-  // if (signature) {
-  //   console.log("HEY LOOK", signature)
-  //   serializedTransaction.push(
-  //     signature.v === 27n ? '0x0' : '0x1', // yParity
-  //     toHex(signature.r),
-  //     toHex(signature.s),
-  //   )
-  // }
 
   return concatHex([
     SuaveTxTypes.ConfidentialRequest,
@@ -312,6 +279,8 @@ export function assertTransactionSuave(
     value,
     maxPriorityFeePerGas,
     maxFeePerGas,
+    confidentialInputs,
+    confidentialInputsHash,
     to,
     r,
     s,
@@ -320,6 +289,12 @@ export function assertTransactionSuave(
   if (chainId && chainId <= 0) throw new Error('invalid chain ID') // TODO: custom err
   if (to && !isAddress(to)) throw new Error('invalid to address') // TODO: custom err
   if (!gasPrice) throw new Error('gasPrice is required')
+
+  if (confidentialInputs && !isHex(confidentialInputs))
+    throw new Error('invalid confidentialInputs') // TODO: custom err
+
+  if (confidentialInputsHash && !isHex(confidentialInputsHash))
+    throw new Error('invalid confidentialInputsHash') // TODO: custom err
 
   if (gas && gas <= 0) throw new Error('invalid gas') // TODO: custom err
 
