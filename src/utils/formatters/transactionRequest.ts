d94a0f484245db357dcd86bae5e44b242baf1ec3
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-11-07 15:27:13 -0800

    re-enable anvil backend for tests

--- src/utils/formatters/transactionRequest.ts
+++ src/utils/formatters/transactionRequest.ts
@@ -20,7 +20,6 @@ export const rpcTransactionType = {
   legacy: '0x0',
   eip2930: '0x1',
   eip1559: '0x2',
-  suave: '0x50',
 } as const
 
 export type FormatTransactionRequestErrorType = ErrorType
