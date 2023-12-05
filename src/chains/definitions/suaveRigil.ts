21fee03e2f980701b9f8f401959f14ce9789962d
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-12-05 13:49:48 -0800

    add support for json-rpc accounts (e.g. metamask) w/ eth_sign

--- src/chains/definitions/suaveRigil.ts
+++ src/chains/definitions/suaveRigil.ts
@@ -1,5 +1,5 @@
 import { createPublicClient } from '../../clients/createPublicClient.js'
-import { type Transport } from '../../clients/transports/createTransport.js'
+import type { Transport } from '../../clients/transports/createTransport.js'
 import { http } from '../../clients/transports/http.js'
 import { type Hex } from '../../types/misc.js'
 import { defineChain } from '../../utils/chain.js'
@@ -40,15 +40,51 @@ export const suaveRigil = /*#__PURE__*/ defineChain(
     },
     contracts: {},
     testnet: true,
-    newWallet: (privateKey: Hex, transport?: Transport) =>
-      getSuaveWallet(
-        {
-          transport: transport ?? defaultTransport,
-          chain: suaveRigil,
+    /** Creates a new SUAVE Wallet instance.
+     * @example
+      // new wallet with a private key
+      import { suaveRigil } from 'viem/chains'
+      import { http } from 'viem'
+      const wallet = suaveRigil.newWallet({
+         transport: http('http://localhost:8545'),
+         privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+      })
+     * @example
+      // new wallet with a json-rpc account using window.ethereum
+      // it's assumed that the wallet is already connected to SUAVE chain
+      import { suaveRigil } from 'viem/chains'
+      import { custom } from 'viem'
+      const wallet = suaveRigil.newWallet({
+        transport: custom(window.ethereum),
+        jsonRpcAccount: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+      })
+     */
+    newWallet: (params: {
+      privateKey?: Hex
+      jsonRpcAccount?: Hex
+      transport?: Transport
+    }) =>
+      getSuaveWallet({
+        transport: params.transport ?? defaultTransport,
+        chain: suaveRigil,
+        privateKey: params.privateKey,
+        jsonRpcAccount: params.jsonRpcAccount && {
+          address: params.jsonRpcAccount,
+          type: 'json-rpc',
         },
-        privateKey,
-      ),
-    newPublicClient: (transport?: any) =>
+      }),
+    /** Creates a new SUAVE Public Client instance.
+     * @example
+      import { suaveRigil } from 'viem/chains'
+      import { http } from 'viem'
+      const client = suaveRigil.newPublicClient(http('http://localhost:8545'))
+     * @example
+      // using window.ethereum
+      import { suaveRigil } from 'viem/chains'
+      import { custom } from 'viem'
+      const client = suaveRigil.newPublicClient(custom(window.ethereum))
+     */
+    newPublicClient: (transport?: Transport) =>
       createPublicClient({
         transport: transport ?? defaultTransport,
         chain: suaveRigil,
--- src/chains/suave/wallet.ts
+++ src/chains/suave/wallet.ts
@@ -2,10 +2,12 @@ import { privateKeyToAccount } from '../../accounts/privateKeyToAccount.js'
 import { sign } from '../../accounts/utils/sign.js'
 import {
   type Chain,
+  type JsonRpcAccount,
   type PrivateKeyAccount,
   type Transport,
   type WalletClient,
   createWalletClient,
+  hexToSignature,
   keccak256,
 } from '../../index.js'
 import { type Hex } from '../../types/misc.js'
@@ -20,6 +22,18 @@ import {
   type TransactionSerializableSuave,
 } from './types.js'
 
+function formatSignature(signature: {
+  r: Hex
+  s: Hex
+  v: bigint
+}): { r: Hex; s: Hex; v: bigint } {
+  return {
+    r: signature.r,
+    s: signature.s,
+    v: signature.v === 27n ? 0n : 1n,
+  }
+}
+
 async function signConfidentialComputeRecord(
   transaction: TransactionSerializableSuave,
   privateKey: Hex,
@@ -30,31 +44,57 @@ async function signConfidentialComputeRecord(
     )
   }
   const serialized = serializeConfidentialComputeRecord(transaction)
-  const { r, s, v } = await sign({ hash: keccak256(serialized), privateKey })
-  const signature = {
-    r,
-    s,
-    v: v === 27n ? 0n : 1n,
-  }
+  const signature = await sign({ hash: keccak256(serialized), privateKey })
   return {
     ...transaction,
-    ...signature,
+    ...formatSignature(signature),
+  }
+}
+
+function getSigningMethod(transport: any, privateKey?: Hex, address?: Hex) {
+  if (transport.type === 'custom') {
+    return async (txRequest: TransactionSerializableSuave) => {
+      const rawSignature = await transport.request({
+        method: 'eth_sign',
+        params: [
+          address,
+          keccak256(serializeConfidentialComputeRecord(txRequest)),
+        ],
+      })
+      const parsedSignature = hexToSignature(rawSignature)
+      return formatSignature(parsedSignature)
+    }
+  } else {
+    if (!privateKey) {
+      throw new Error('privateKey is required for non-custom transports')
+    }
+    return async (txRequest: TransactionSerializableSuave) => {
+      return await signConfidentialComputeRecord(txRequest, privateKey)
+    }
   }
 }
 
 export function getSuaveWallet<
   TTransport extends Transport,
   TChain extends Chain,
->(
-  params: { transport: TTransport; chain: TChain },
-  privateKey: Hex,
-): WalletClient<
+>(params: {
+  transport: TTransport
+  chain: TChain
+  jsonRpcAccount?: JsonRpcAccount
+  privateKey?: Hex
+}): WalletClient<
   TTransport,
   TChain,
   PrivateKeyAccount // TODO: generalize account types (required to make metamask transport work)
 > {
+  if (!params.jsonRpcAccount && !params.privateKey) {
+    throw new Error("Must provide either 'account' or 'privateKey'")
+  }
+  if (params.jsonRpcAccount && params.privateKey) {
+    throw new Error("Cannot provide both 'account' and 'privateKey'")
+  }
   return createWalletClient({
-    account: privateKey ? privateKeyToAccount(privateKey) : undefined,
+    account: params.jsonRpcAccount ?? privateKeyToAccount(params.privateKey!),
     transport: params.transport,
     chain: params.chain,
   }).extend((client) => ({
@@ -80,16 +120,21 @@ export function getSuaveWallet<
     async signTransaction(txRequest: TransactionRequestSuave) {
       if (txRequest.type === SuaveTxTypes.ConfidentialRequest) {
         const confidentialInputs = txRequest.confidentialInputs || '0x'
+
+        // determine signing method based on transport type
+        const signCcr = getSigningMethod(
+          client.transport,
+          params.privateKey,
+          client.account.address,
+        )
         const presignTx = {
           ...txRequest,
           type: SuaveTxTypes.ConfidentialRecord,
           confidentialInputsHash: keccak256(confidentialInputs),
           chainId: txRequest.chainId ?? suaveRigil.id,
         }
-        const { r, s, v } = await signConfidentialComputeRecord(
-          presignTx,
-          privateKey,
-        )
+        const sig = await signCcr(presignTx)
+        const { r, s, v } = sig
         return serializeConfidentialComputeRequest({
           ...presignTx,
           confidentialInputs,
