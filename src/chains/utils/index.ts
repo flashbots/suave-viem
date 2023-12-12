f65bd51c2a8a188e6339990be0156af2a03471ae
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2023-12-11 16:05:30 -0800

    export suave types w/ other special types

--- src/chains/utils/index.ts
+++ src/chains/utils/index.ts
@@ -43,3 +43,28 @@ export type {
   OptimismTransactionReceipt,
   OptimismTransactionReceiptOverrides,
 } from '../optimism/types.js'
+
+export { formattersSuave } from '../suave/formatters.js'
+export type {
+  ConfidentialComputeRecord,
+  ConfidentialComputeRecordRpc,
+  RpcTransactionReceiptSuave,
+  RpcTransactionRequestSuave,
+  RpcTransactionSuave,
+  SuaveBlock,
+  SuaveBlockOverrides,
+  SuaveRpcBlock,
+  SuaveTransactionReceiptOverrides,
+  SuaveTxRequestType,
+  SuaveTxType,
+  TransactionReceiptSuave,
+  TransactionRequestSuave,
+  TransactionSerializableEIP2930,
+  TransactionSerializableSuave,
+  TransactionSerializedSuave,
+  TransactionSuave,
+} from '../suave/types.js'
+export {
+  SuaveTxTypes,
+  SuaveTxRequestTypes,
+} from '../suave/types.js'
